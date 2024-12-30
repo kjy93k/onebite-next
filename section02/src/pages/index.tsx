f8ef2894f407d53079c9c74f60cef53189b1de83
jy.kim <kjy93k@naver.com>
2024-12-30 19:23:14 +0900

    2.6 스타일링

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,10 @@
+// css module
+import style from "./index.module.css";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1 className={style.h1}>Index</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
