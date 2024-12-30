0664c593535fd07c4ac1b2152ea57fc267b8e883
jy.kim <kjy93k@naver.com>
2024-12-30 19:48:34 +0900

    2.7 글로벌 레이아웃 설정하기

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,30 +1,11 @@
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    // router.replace("/test");
-    router.push("/test");
-  };
+import GlobalLayout from "@/components/global-layout";
 
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Link href={"/"}>Index</Link>
-      &nbsp;
-      <Link href={"/search"} prefetch={false}>
-        Search
-      </Link>
-      &nbsp;
-      <Link href={"/book/1"}>Book 1</Link>
-      <div>
-        <button onClick={onClickButton}>/test 페이지로 이동</button>
-      </div>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
