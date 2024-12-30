31cd36dd869e33b4c8b83463ee8a403c3a2756e8
jy.kim <kjy93k@naver.com>
2024-12-30 18:53:17 +0900

    2.4 프리패칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,23 +1,30 @@
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const onClickButton = () => {
     // router.replace("/test");
     router.push("/test");
   };
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
   return (
     <>
       <Link href={"/"}>Index</Link>
       &nbsp;
-      <Link href={"/serch"}>Serch</Link>
+      <Link href={"/search"} prefetch={false}>
+        Search
+      </Link>
       &nbsp;
-      <Link href={"/bool/1"}>Book 1</Link>
+      <Link href={"/book/1"}>Book 1</Link>
       <div>
         <button onClick={onClickButton}>/test 페이지로 이동</button>
       </div>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
