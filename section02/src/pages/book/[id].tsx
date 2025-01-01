6aab794a800e2e7383823ebdf46dc9efbfbdc4a7
jy.kim <kjy93k@naver.com>
2025-01-01 21:10:19 +0900

    2.15 SSG 3. 동적 경로에 적용하기

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -2,24 +2,35 @@ import { useRouter } from "next/router";
 import books from "@/mock/books.json";
 import { BookData } from "@/types";
 import style from "./[id].module.css";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {
+  GetServerSidePropsContext,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+  InferGetStaticPropsType,
+} from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = books[0];
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: false, //paths에 없는 접근은 없는페이지로 취급
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
   return { props: { book } };
 };
-const Page = ({
-  book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const router = useRouter();
-  // const { id } = router.query;
+const Page = ({ book }: InferGetStaticPropsType<typeof getStaticProps>) => {
   if (!book) return "문제가 발생했습니다 다시 시도해주세요";
+
   const {
     id,
     title,
