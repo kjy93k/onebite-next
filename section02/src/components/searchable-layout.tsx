6a826cb35f959cd91d336e206277dd4d875ad85e
jy.kim <kjy93k@naver.com>
2025-01-02 16:09:35 +0900

    2.19 SEO 설정하기

--- section02/src/components/searchable-layout.tsx
+++ section02/src/components/searchable-layout.tsx
@@ -1,13 +1,14 @@
 import { ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import style from "./searchable-layout.module.css";
+import Head from "next/head";
 const SearchableLayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const [search, setSearch] = useState("");
   const { q } = router.query as { q: string };
 
   useEffect(() => {
-    setSearch(q);
+    setSearch(q || "");
   }, [q]);
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,19 +25,30 @@ const SearchableLayout = ({ children }: { children: ReactNode }) => {
   };
 
   return (
-    <div>
-      <div className={style.searchbar_container}>
-        <input
-          type="text"
-          value={search}
-          onKeyDown={onKeyDown}
-          onChange={onChangeSearch}
-          placeholder="검색어를 입력하세요..."
+    <>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="힌입북스 - 검색결과" />
+        <meta
+          property="og:description"
+          content="한입북스에 등록된 도서들을 만나보세요"
         />
-        <button onClick={onSubmit}>검색</button>
+      </Head>{" "}
+      <div>
+        <div className={style.searchbar_container}>
+          <input
+            type="text"
+            value={search}
+            onKeyDown={onKeyDown}
+            onChange={onChangeSearch}
+            placeholder="검색어를 입력하세요..."
+          />
+          <button onClick={onSubmit}>검색</button>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </>
   );
 };
 export default SearchableLayout;
--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -9,6 +9,7 @@ import {
   InferGetStaticPropsType,
 } from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
+import Head from "next/head";
 
 const mockData = books[0];
 
@@ -36,7 +37,23 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 
 const Page = ({ book }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter();
-  if (router.isFallback) return "로딩 중";
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="힌입북스" />
+          <meta
+            property="og:description"
+            content="한입북스에 등록된 도서들을 만나보세요"
+          />
+        </Head>
+        <div>로딩중입니다</div>
+      </>
+    );
+  }
+  if (!book) return "문제가 발생했습니다. 다시 시도하세요";
 
   const {
     id,
@@ -48,20 +65,28 @@ const Page = ({ book }: InferGetStaticPropsType<typeof getStaticProps>) => {
     coverImgUrl,
   }: BookData = book;
   return (
-    <div className={style.container}>
-      <div
-        className={style.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} />
+    <>
+      <Head>
+        <title>{title}, 한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style.container}>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
       </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
-      </div>
-      <div className={style.description}>{description}</div>
-    </div>
+    </>
   );
 };
 
