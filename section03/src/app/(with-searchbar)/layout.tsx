512e0ed990d4600a22e0d270843924ecdde3bc9d
jy.kim <kjy93k@naver.com>
2025-01-06 20:59:10 +0900

    5.5 클라이언트 라우터 캐시

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,8 @@ import Searchbar from "@/components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* <div>{new Date().toLocaleString()}</div> */}
+      {/* 클라이언트 라우트 캐시에 캐싱됨, 새로고침 시 사라짐 */}
       <Suspense fallback={<div>Loading ...</div>}>
         <Searchbar />
       </Suspense>
