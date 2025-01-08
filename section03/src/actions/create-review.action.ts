62ae09ac7e1d86b56c45d6cc6794f785698bf554
jy.kim <kjy93k@naver.com>
2025-01-08 17:39:32 +0900

    7.4 리뷰 재검증 구현하기

--- section03/src/actions/create-review.action.ts
+++ section03/src/actions/create-review.action.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
+
 const createReviewAction = async (formData: FormData) => {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -15,6 +18,9 @@ const createReviewAction = async (formData: FormData) => {
       }
     );
     console.log(res.status);
+    // 페이지 내 모든 캐시를 무효화 함
+    // 데이터 캐시, 풀라우트캐시도 무효화(PURGE)되고 데이터캐시와 달리 풀라우트 캐시는 업데이트되지 않음
+    revalidatePath(`/book/${bookId}`);
   } catch (e) {
     console.error(e);
     return;
