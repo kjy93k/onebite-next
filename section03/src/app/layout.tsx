d99ca41c2184818e0404d8158db7b208276ad2c6
jy.kim <kjy93k@naver.com>
2025-01-10 18:14:47 +0900

    8.3 페러렐 & 인터셉터 라우트

--- section03/src/app/layout.tsx
+++ section03/src/app/layout.tsx
@@ -22,8 +22,10 @@ const Footer = async () => {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="ko">
@@ -35,6 +37,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </div>
+        {modal}
         <div id="modal-root"></div>
       </body>
     </html>
